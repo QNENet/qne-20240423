4eea35275359edb7a5281db59543fc8200132266
paulf <paulf@qnenet.com>
2024-05-11 06:32:35 +1000

    feat: Add generated theme and flow files

--- src/main/java/com/qnenet/qne/Application.java
+++ src/main/java/com/qnenet/qne/Application.java
@@ -24,6 +24,7 @@ import com.qnenet.qne.system.utils.QThreadUtils;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 // import org.springframework.boot.SpringApplication;
 // import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -50,14 +51,14 @@ import com.vaadin.flow.theme.Theme;
  * and some desktop browsers.
  */
 @SpringBootApplication
-@Theme(value = "qneTheme")
+@Theme(value = "qneTheme", variant = Lumo.LIGHT)
 @PWA(
         name = "Quick N Easy",
         shortName = "QNE",
         offlinePath = "offline.html",
         offlineResources = {"images/offline.png"}
 )
-public class QNEApplication implements AppShellConfigurator {
+public class Application implements AppShellConfigurator {
 
     private static SpringApplication app;
     private static ConfigurableApplicationContext appCtx;
@@ -77,27 +78,27 @@ public class QNEApplication implements AppShellConfigurator {
     private static void runApp() {
         QThreadUtils.showThreadName("Application");
         Security.addProvider(new BouncyCastleProvider());
-        Path installPath = Paths.get(System.getProperty("user.home"),
-                QSysConstants.APP_NAME, "install");
-        Path systemPortFilePath = Paths.get(installPath.toString(), "systemPort.prop");
+        // Path installPath = Paths.get(System.getProperty("user.home"),
+        //         QSysConstants.APP_NAME, "install");
+        // Path systemPortFilePath = Paths.get(installPath.toString(), "systemPort.prop");
 
-        String portInfoStr = null;
-        try {
-            if (Files.notExists(systemPortFilePath)) {
-                portInfoStr = String.valueOf(QNetworkUtils.getFreePort());
-                FileUtils.writeStringToFile(systemPortFilePath.toFile(), portInfoStr, Charset.defaultCharset());
-            } else {
-                portInfoStr = FileUtils.readFileToString(systemPortFilePath.toFile(), Charset.defaultCharset());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        String portStr = portInfoStr.split(",")[0].trim();
+        // String portInfoStr = null;
+        // try {
+        //     if (Files.notExists(systemPortFilePath)) {
+        //         portInfoStr = String.valueOf(QNetworkUtils.getFreePort());
+        //         FileUtils.writeStringToFile(systemPortFilePath.toFile(), portInfoStr, Charset.defaultCharset());
+        //     } else {
+        //         portInfoStr = FileUtils.readFileToString(systemPortFilePath.toFile(), Charset.defaultCharset());
+        //     }
+        // } catch (IOException e) {
+        //     throw new RuntimeException(e);
+        // }
+        // String portStr = portInfoStr.split(",")[0].trim();
 
-        app = new SpringApplication(QNEApplication.class);
-        app.setHeadless(false);
+        app = new SpringApplication(Application.class);
+ /*        app.setHeadless(false);
         app.setDefaultProperties(Collections.singletonMap("server.port", portStr));
-    }
+  */   }
 
     static void shutdown() {
         if (appCtx.isActive()) appCtx.close();
